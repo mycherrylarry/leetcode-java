47a5b9587a4a8abe2a1976e19f2a7f92900e928a
changle <mycherrylarry@gmail.com>
2014-11-05 17:49:42 +0900

    INIT

---
+++ src/leetcode/find_minimum_in_rotated_sorted_array/Solution.java
@@ -0,0 +1,20 @@
+package leetcode.find_minimum_in_rotated_sorted_array;
+
+/**
+ * Problem Description:
+ * Solution:
+ * Note:
+ *
+ * Time Complexity:
+ * Space Complexity:
+ * Result:
+ */
+public class Solution {
+}
+
+class TestCase {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println("hello");
+    }
+}
---
+++ src/leetcode/template/Solution.java
@@ -0,0 +1,20 @@
+package leetcode.template;
+
+/**
+ * Problem Description:
+ * Solution:
+ * Note:
+ *
+ * Time Complexity:
+ * Space Complexity:
+ * Result:
+ */
+public class Solution {
+}
+
+class TestCase {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println("hello");
+    }
+}
